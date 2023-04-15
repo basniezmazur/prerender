d6dec282f827e6518900b1bca97ab5d28fe60eef
basniezmazur <122865973+basniezmazur@users.noreply.github.com>
2023-04-15 15:32:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    chromeLocation: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
+    chromeLocation: './lib/browsers/'
 });
 
 server.use(prerender.sendPrerenderHeader());
