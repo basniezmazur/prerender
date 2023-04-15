c822497232d93f19153edd275d23314b6806926e
basniezmazur <122865973+basniezmazur@users.noreply.github.com>
2023-04-15 15:40:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    chromeLocation: './lib/browsers/'
+  chromeLocation: './lib/browsers/',
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
 });
 
 server.use(prerender.sendPrerenderHeader());
