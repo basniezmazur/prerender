53e245043bc8305eb5ed103ca98ce57333f40607
basniezmazur <122865973+basniezmazur@users.noreply.github.com>
2023-04-15 13:52:13 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+var prerender = require('./lib');
+
+var server = prerender({
+    chromeLocation: '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
+});
+
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.browserForceRestart());
+// server.use(prerender.blockResources());
+server.use(prerender.addMetaTags());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+
+
+server.start();
